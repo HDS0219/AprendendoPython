891e829f8900f655ca902e8aea7523ac831b5c58
HDS0219 <dan1902heller@gmail.com>
2024-05-21 18:15:12 -0300

    comentarios atualizados

--- src/Herança(POO).py
+++ src/Herança(POO).py
@@ -1,5 +1,5 @@
 class NPC:  # Classe pai / SuperClasse
-    def __init__(self, nome, time, forca, municao):
+    def __init__(self, nome, time, forca, municao): #construtor
         self.nome = nome
         self.time = time
         self.forca = forca
@@ -7,36 +7,36 @@ class NPC:  # Classe pai / SuperClasse
         self.vivo = True
         self.energia = 100
 
-    def info(self):
+    def info(self): #função para exibir informações
         print(f"Nome: {self.nome}\nTime: {self.time}\nForça: {self.forca}\nMunição: {self.municao}\n"
               f"Vivo: {"Sim" if self.vivo else "Não"}\nenergia: {self.energia}")
         print("===========================================")
 
-
-class Soldado(NPC):
-    def __init__(self, nome, time):
+#classes filhos utilizam dentro do parentese a classe pai para herdar suas caracteristias
+class Soldado(NPC): #Classe filho
+    def __init__(self, nome, time): #construtor da classe filho pegando informações da classe pai e tranferindo a si mesmo informações que o difere da superclasse
         self.forca = 200
         self.municao = 200
         super().__init__(nome, time, self.forca, self.municao)
 
 
-class Guarda(NPC):
+class Guarda(NPC): #classe filho
     def __init__(self, nome, time):
         self.forca = 100
         self.municao = 20
         super().__init__(nome, time, self.forca, self.municao)
 
 
-class Elite(NPC):
+class Elite(NPC): #classe filho
     def __init__(self, nome, time):
         self.forca = 400
         self.municao = 500
         super().__init__(nome, time, self.forca, self.municao)
 
-    def inf(self):
+    def inf(self): #herdando uma função da função pai
         super().info()
 
-
+#Exibindo informações das classes
 s1 = Guarda("Player1", 1)
 s2 = Soldado("Player2", 2)
 s3 = Elite("Minecraft", 1)
@@ -52,4 +52,4 @@ s2.info()
 s3.info()
 s4.info()
 s5.info()
-s6.inf() #pegan
+s6.inf() #pegando informações atraves da herança "info"
