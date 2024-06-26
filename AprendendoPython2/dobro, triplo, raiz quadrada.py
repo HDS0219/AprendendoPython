72dab8214e7460ec96ae4d09de5994740589d3c4
HDS0219 <dan1902heller@gmail.com>
2024-06-26 17:16:40 -0300

    Aula de dobro, triplo e raiz

---
+++ AprendendoPython2/dobro, triplo, raiz quadrada.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+num = int(input("Digite um número: "))
+print(f"O dobro de {num} vale {num * 2}\n O triplo de {num} vale {num * 3}\n A raiz quadrada de {num} é {sqrt(num):.2f}") # forma 1
+
+print("==========================")
+
+dobro = num * 2
+triplo = num * 3
+raiz = num ** (1/2)
+
+print(f"O dobro de {num} vale {dobro}\n O triplo de {num} vale {triplo}\n A raiz quadrada de {num} é {raiz:.2f}") # forma 2
