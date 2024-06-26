a0158ad52971c142df20ae50be92690b6e8aef32
HDS0219 <dan1902heller@gmail.com>
2024-06-26 16:55:30 -0300

    Criado pasta para armazenar mais aprendizado

---
+++ AprendendoPython2/numero antecessor e sucessor.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um número: "))
+print(f"Analisando o valor {num}, seu antecessor é {num - 1} e seu sucessor é {num + 1}") # forma número 1
+
+print("===============")
+
+antecessor = num - 1
+sucessor = num + 1
+print(f"Analisando o valor {num}, seu antecessor é {antecessor} e seu sucessor é {sucessor}")  # forma número 2
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Python
 Um repositório criado para documentar minha jornada e aprendizado em Python, facilitando também a minha busca a lições já aprendidas.
+
+## Cursos que estou aprendendo:
+- Curso em vídeo
+- Jornada do dev
