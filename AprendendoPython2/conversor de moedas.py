19542245c80654fcebd4b5121a5038c230ac8753
HDS0219 <dan1902heller@gmail.com>
2024-07-01 16:58:48 -0300

    tristeza adicionada

---
+++ AprendendoPython2/conversor de moedas.py
@@ -0,0 +1,6 @@
+moeda = float(input("Digite a quantia em dinheiro que deseja converter: "))
+
+dolar = moeda / 5.65 #infelizmente aumentar(ou felizmente diminuir) caso haja mudanças no dolar :(
+
+
+print(f"a conversão dá: {dolar:.2f} dolare(s)")
