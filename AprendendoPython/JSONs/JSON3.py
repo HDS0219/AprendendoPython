7dd5bbb93e78e6b379b59f1c3ddc61238fb03c71
HDS0219 <dan1902heller@gmail.com>
2024-07-09 15:37:52 -0300

    new python file created

--- AprendendoPython/JSONs/JSON3.py
+++ AprendendoPython/JSONs/JSON3.py
@@ -1,8 +1,6 @@
 import json
 
-jogador = '{"nome": "Bruno","time": "aviadores","vivo": "True","energia": 100,"mochila": ["pederneira", "corda", "linha", "arame"],"aeronaves": [{"tipo": "transporte", "habilidade": 80},{"tipo": "ataque", "habilidade": 100},{"tipo": "reconhecimento", "habilidade": 50}]}'
-
-'''import json
+'''
 
 jogador = {
     "nome": "Bruno",
@@ -18,3 +16,45 @@ jogador = {
 }
 '''
 
+jogador_j = '{"nome": "Bruno","time": "aviadores","vivo": "True","energia": 100,"mochila": ["pederneira", "corda", "linha", "arame"],"aeronaves": [{"tipo": "transporte", "habilidade": 80},{"tipo": "ataque", "habilidade": 100},{"tipo": "reconhecimento", "habilidade": 50}]}'
+
+jogador = json.loads(jogador_j)
+
+
+#chaves
+for c in jogador:
+    print(c)
+
+print("="*30)
+#itens
+for i in jogador.items():
+    print(i)
+
+print("="*30)
+#valores
+for v in jogador.values():
+    print(v)
+
+print("="*30)
+#nome do jogador
+print(jogador["nome"])
+
+print("="*30)
+#time
+print(jogador["time"])
+
+print("="*30)
+#itens da mochila
+
+for im in jogador["mochila"]:
+    print(im)
+
+print("="*30)
+#aeronaves
+for a in jogador["aeronaves"]:
+    print(a)
+
+print("="*30)
+#tipos de aeronaves
+for a in jogador["aeronaves"]:
+    print(f"{a["tipo"]} - {a["habilidade"]}")
